949df9ba5c76ee47d5e46c9240c35b5d8d394eda
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-12-26 21:31:21 +0100

    feat: db conn

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+const databaseUrl = process.env.DATABASE_URL
+  || 'mongodb://localhost:27017/test';
+
+console.log('Connecting' + databaseUrl)
+
+mongoose.connect(databaseUrl);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  imageSchema.find().then((data) => {
+    console.log(data);
+    res.render('index', { title: 'Express', users: data })
+  });
+  ;
 });
 
 module.exports = router;
